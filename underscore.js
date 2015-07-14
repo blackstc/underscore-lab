f7bca40e71bcda2eb6072813da233aafca2d64fa
Blackstock <charlieblackstock@gmail.com>
2015-07-14 17:02:55 -0600

    added several more functions

--- underscore.js
+++ underscore.js
@@ -1,87 +1,120 @@
-//*average
-function average(numArray) {
-  var total = 0
+var _ = {
 
-  for (var i = 0; i < numArray.length; i++) {
-    total += numArray[i];
-  };
+  //*average
+  average: function(numArray) {
+    var total = 0;
 
-  return total / numArray.length;
-}
+    for (var i = 0; i < numArray.length; i++) {
+      total += numArray[i];
+    }
+
+    return total / numArray.length;
+  },
 
 
-//*contains
-function contains(numArray, searchNum) {
-  var search = 0;
+  //*contains
+  contains: function(numArray, searchNum) {
+    var search = 0;
 
-  for (var i = 0; i < numArray.length; i++) {
-    if (numArray[i] === searchNum) {
-      search++
-    };
+    for (var i = 0; i < numArray.length; i++) {
+      if (numArray[i] === searchNum) {
+        search++;
+      }
+    }
+    if (search >= 1) {
+      return true;
+    } else {
+      return false;
+    }
+  },
+
+
+  //*first
+  first: function(numArray) {
+    return numArray[0];
+  },
+
+
+  //*last
+  last: function(numArray) {
+    var lastNum = numArray.length - 1;
+    return numArray[lastNum];
+  },
+
+
+  //*max
+  max: function(numArray) {
+    var maximum = numArray[0];
+    for (var i = 1; i < numArray.length; i++) {
+      if (numArray[i] > maximum) {
+        maximum = numArray[i];
+      }
+    }
+    return maximum;
+  },
+
+
+  //*min
+  min: function(numArray) {
+    var minimum = numArray[0];
+
+    for (var i = 1; i < numArray.length; i++) {
+      if (numArray[i] < minimum) {
+        minimum = numArray[i];
+      }
+    }
+    return minimum;
+  },
+
+
+  //*shuffle
+  shuffle: function(numArray) {
+    var output = [];
+    var random = 0;
+    var workingArray = numArray.slice(0);
+    var current = workingArray.length;
+
+    while (workingArray.length > 0) {
+      random = Math.floor(Math.random() * workingArray.length);
+      output.push(workingArray.splice(random, 1)[0]);
+    }
+    return output;
+  },
+
+
+  //*sample
+  sample: function(numArray, sampleNum) {
+    var output = [];
+    var random = 0;
+    var workingArray = numArray.slice(0);
+    var current = workingArray.length;
+    var newSample = numArray.length - sampleNum;
+
+    while (workingArray.length > newSample) {
+      random = Math.floor(Math.random() * workingArray.length);
+      output.push(workingArray.splice(random, 1)[0]);
+    }
+    return output;
+  },
+
+
+  //*difference
+  difference: function(arr1, arr2) {
+    var result = [];
+    var compare = 0;
+    var first = arr1[1];
+
+    for(j = 0; j < arr1.length; j++) {
+        for (i = 0; i < arr2.length; i++) {
+            if(arr1[j] !== arr2[i]) {
+                result.push(arr1[i]);
+            }
+        }
+    }
+
+
+    return result;
   }
-  if (search >= 1) {
-    return true;
-  } else {
-    return false;
-  };
-}
+};
 
-//*first
-function first(numArray) {
-  return numArray[0];
-}
-
-//*last
-function last(numArray) {
-  var lastNum = numArray.length - 1;
-  return numArray[lastNum];
-}
-
-//*max
-function max(numArray) {
-  var maximum = numArray[0];
-  for (var i = 1; i < numArray.length; i++) {
-    if (numArray[i] > maximum) {
-      maximum = numArray[i];
-    };
-  };
-  return maximum;
-}
-
-
-//*min
-function min(numArray) {
-  var minimum = numArray[0];
-
-  for (var i = 1; i < numArray.length; i++) {
-    if (numArray[i] < minimum) {
-      minimum = numArray[i];
-    };
-  };
-  return minimum;
-}
-
-
-//*shuffle
-function shuffle(numArray) {
-  var output = [];
-  var random = 0;
-  var workingArray = numArray.slice(0);
-  var current = workingArray.length;
-
-  while (workingArray.length > 0) {
-    random = Math.floor(Math.random() * workingArray.length);
-    output.push(workingArray.splice(random, 1)[0]);
-  }
-  return output;
-}
-
-
-//*sample
-function sample(numArray, sampleNum) {
-  var output = [];
-
-  for (var i = 0; i < numArray.length; i++) {
-    numArray[i]
-  }
-}
+_.difference([1, 2, 3, 4, 5], [5, 2, 10]);
